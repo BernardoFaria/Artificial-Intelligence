91999f28394287a69f035b9bf8079dabcfb1fb29
OcarinaRedcoat <98.ricardo.caetano@gmail.com>
2019-10-18 21:27:42 +0100

    Ex 1

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -1,20 +1,79 @@
 import math
 import pickle
 import time
+import sys
 
-  
 class SearchProblem:
+  def __init__(self, goal, model, auxheur=[]):
+    self.goal = goal #obejtivo
+    self.model = model #grafo com as transicoes entre localizacoes
+    self.auxheur = auxheur
 
-  def __init__(self, goal, model, auxheur = []):
-    ##
-    ## to implement
-    ## 
-    pass
+    self.g_agent1 = 0
+    self.g_agent2 = 0
+    self.g_agent3 = 0
 
-  def search(self, init, limitexp = 2000, limitdepth = 10, tickets = [math.inf,math.inf,math.inf]):
-    ##
-    ## to implement
-    ##
-    return []
+    self.queue = []
+  def search(self, init, limitexp=2000, limitdepth=10, tickets=[math.inf, math.inf, math.inf]):
+    def cost(agent): # agent represents the index, agent 1 is index 0, 2 is index 1 and 3 is index 2
+    #FIXME:
+        if (agent == 0):
+            self.g_agent1 += 1
+            return self.g_agent1
+        elif (agent == 1):
+            self.g_agent2 += 1
+            return self.g_agent2
+        else:
+            self.g_agent3 += 1
+            return self.g_agent3
 
-    
+    def heuristic(node, destiny):  #send the tupple of coords of the nodes in auxheur
+        return math.sqrt(((destiny[0]-node[0])**2)+((destiny[1]-node[1])**2))
+
+    def total_cost(node, agent): # function f that reperesent g + h
+        return heuristic(self.auxheur[node-1] , self.auxheur[self.goal[agent]-1])
+
+    def is_goal(node):
+        return node == self.goal[0]
+
+    def successors(node):
+        succ = self.model[node]
+        for i in range (len(succ)):
+            cost = total_cost(succ[i][1], 0)
+            succ[i].append(cost)
+            self.queue.append(succ[i])
+            # succ[i].append(self.auxheur[succ[i][1]-1])
+            succ[i].append(self.auxheur[self.goal[0]-1])
+        return succ
+
+
+    shortest_path = [[[], init]]
+
+    if(is_goal(init)):
+        return shortest_path
+
+
+    for i in range (len(init)):
+        successors(init[i])
+        #self.queue = successors(init[i])
+
+    #for i in range(5):
+    while (is_goal(shortest_path[-1][1])==False):
+        smallest_cost_node = []
+        for i in range (len(self.queue)):
+            if (i == 0):
+                smallest_cost_node = [self.queue[i][0], self.queue[i][1], self.queue[i][2]]
+            else:
+                if (smallest_cost_node[2] > self.queue[i][2]):
+                    smallest_cost_node[0] = self.queue[i][0]
+                    smallest_cost_node[1] = self.queue[i][1]
+                    smallest_cost_node[2] = self.queue[i][2]
+
+
+        next_node = [smallest_cost_node[0], smallest_cost_node[1]]
+        shortest_path.append(next_node)
+        successors(next_node[1])
+        # print(self.queue)
+        # print(shortest_path)
+
+    return shortest_path
--- seePickle.py
+++ seePickle.py
@@ -7,7 +7,7 @@ file.write(str(example_in1))
 file.close()
 
 
-pickle_in2 = open("mapasgraph.pickle", "rb")
+pickle_in2 = open("mapasgraph2.pickle", "rb")
 example_in2 = pickle.load(pickle_in2)
 file = open("mapasgraph.pickle.out", "w+")
 file.write(str(example_in2))
