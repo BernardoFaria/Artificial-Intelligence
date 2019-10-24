ce5db43f416d2679cd704fd054bad46c56d9db87
OcarinaRedcoat <98.ricardo.caetano@gmail.com>
2019-10-24 23:45:49 +0100

    Again, the shit is all f*cked up, but at least output format is right, well not the output

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -1,31 +1,28 @@
 import math
 import pickle
 import time
+from copy import deepcopy
 
 class SearchProblem:
   def __init__(self, goal, model, auxheur=[]):
-    self.goal = goal #obejtivo
-    self.model = model #grafo com as transicoes entre localizacoes
+    self.goal = goal
+    self.model = model
     self.auxheur = auxheur
 
-    #The set of discovered nodes that may need to be (re-)expanded.
-    #Initially, only the start node is known.
     self.openSet = [list(), list(), list()]
 
-    #For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start to n currently known.
     self.cameFrom = [dict(), dict(), dict()]
 
-    #For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
     self.gScore = [dict(), dict(), dict()]
 
     self.already_visited = list()
 
   def search(self, init, limitexp=2000, limitdepth=10, tickets=[math.inf, math.inf, math.inf]):
 
-    def heuristic(node, destiny):  #send the tupple of coords of the nodes in auxheur
+    def heuristic(node, destiny):
         return math.sqrt(((destiny[0]-node[0])**2)+((destiny[1]-node[1])**2))
 
-    def total_cost(node, goal, agent): # function f that reperesent g + h
+    def total_cost(node, goal, agent):
         return self.gScore[agent].get(node, math.inf) + heuristic(self.auxheur[node-1] , self.auxheur[goal-1])
 
     def is_goal(node1, node2, node3, agent):
@@ -52,21 +49,50 @@ class SearchProblem:
         return small
 
 
-    def reconstruct_path(current, agent):
+    def reconstruct_path(current, agent = None, flag = 0):
         total_path = [[[], [current]]]
 
+        if (flag == 0):
+            transport0 = go_next(self.already_visited[-2][0], self.already_visited[-1][0])
+            transport1 = go_next(self.already_visited[-2][1], self.already_visited[-1][1])
+            transport2 = go_next(self.already_visited[-2][2], self.already_visited[-1][2])
+            node = [transport0[0], transport1[0], transport2[0]]
 
-        while current in self.cameFrom[agent]:
-            last_node = current
-            current = self.cameFrom[agent][current]
-            next = go_next(current, last_node)
-            total_path[0][0].extend(next)
-            total_path = [[[], [current]]] + total_path
-        # print(total_path)
-        return total_path
+
+            aux = deepcopy(self.already_visited)
+
+            aux.remove(self.goal)
+            result = [[[], init]]
+
+            current = aux[-1]
+
+            for index in range(len(self.already_visited)-1):
+                transports_list = list()
+                for i in range(len(init)):
+
+                    transports = go_next(self.already_visited[index][i],self.already_visited[index + 1][i])
+                    transports_list.extend(transports)
+
+                result.append([transports_list])
+
+                result[index + 1].append(self.already_visited[index+1])
+
+            return result
+
+        if (flag == 1):
+
+            while current in self.cameFrom[0]:
+
+                last_node = current
+                current = self.cameFrom[0][current]
+                next = go_next(current, last_node)
+                total_path[0][0].extend(next)
+                total_path = [[[], [current]]] + total_path
+            return total_path
 
 
     def go_next(f, to):
+
         for n in self.model[f]:
             if n[1] == to:
                 return [n[0]]
@@ -76,13 +102,14 @@ class SearchProblem:
     def check_tickets(current, neighbor, agent):
         check_tickets = [0, 0, 0]
 
-        path = reconstruct_path(current, agent)
+        path = reconstruct_path(current, agent, 1)
 
         for step in path:
 
             if step[0]:
                 check_tickets[step[0][0]] += 1
         next = go_next(current, neighbor)
+        
         check_tickets[next[0]] += 1
         return all([check_tickets[i] <= tickets[i] for i in range(3)])
 
@@ -137,19 +164,25 @@ class SearchProblem:
                     self.openSet[i].remove(current[i])
 
                 self.already_visited.append(current)
-                # print("WHAZAAAAAA")
+
                 flag = 1
             elif (c == -1 and (current in self.already_visited)):
                 for i in range(len(init)):
                     self.openSet[i].remove(current[i])
 
+
         if (is_goal(current[0], current[1], current[2], i)):
-            print("Entrou no is_goal:" + str([current[0], current[1], current[2]]))
-            for i in range(len(init)):
-                result = reconstruct_path(current[i], i)
+
+            result = [list(), list(), list()]
+            if (len(init) == 1):
+                result = reconstruct_path(current[0], 0, 1)
+            else:
+                result = reconstruct_path(current, i)
+
             return result
 
         for i in range(len(init)):
+
             for neighbor in self.model[current[i]]:
 
                 neighbor = neighbor[1]
@@ -157,6 +190,7 @@ class SearchProblem:
                 next_g = self.gScore[i][current[i]] + 1
 
                 if self.gScore[i].get(neighbor, math.inf) > next_g:
+
                     if not check_tickets(current[i], neighbor, i):
                         continue
 
@@ -167,6 +201,7 @@ class SearchProblem:
 
                     self.openSet[i].append(neighbor)
                     limitexp -= 1
+
         limitdepth -= 1
 
     return []
